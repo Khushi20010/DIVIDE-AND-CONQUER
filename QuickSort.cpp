2785e51be97cfd419488de5a86de604a837a1b38
Anushka Yadav <anushkayadav382@gmail.com>
2025-08-25 21:47:55 +0530

    Added Quick Sort and Merge Sort implementations in C++

---
+++ QuickSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int partition(int arr[] , int low, int high){
+    int pivot = arr[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            swap(arr[i] , arr[j]);
+        }
+    }
+    swap(arr[i+1] , arr[high]);
+    return i+1;
+}
+void QuickSort(int arr[] , int low , int high){
+    if(low<high){
+        int pi = partition(arr , low , high);
+         QuickSort(arr , low , pi-1);
+          QuickSort(arr ,pi+1 , high);
+    }
+}
+
+int main(){
+    int arr[] = {23,43,65,676,7,7,7,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+     QuickSort(arr , 0 , n-1);
+     cout<<"All sorted elements are :  "<<endl;
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "<<endl;
+     }
+     return 0;
+}
